d0a3f2994fd3590fb0009ed9760b1fcd3762ccef
juanortegon1016 <100365995+juanortegon1016@users.noreply.github.com>
2023-04-22 11:21:30 -0500

    Challenge 14

---
+++ Challenge 14/src/firebase/config.js
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDcTiuA1-9ABI7yIb9PIetKHJl__Sl_LRA",
+  authDomain: "challenges-cfb0a.firebaseapp.com",
+  projectId: "challenges-cfb0a",
+  storageBucket: "challenges-cfb0a.appspot.com",
+  messagingSenderId: "286648209097",
+  appId: "1:286648209097:web:c025d1df77be6844c38718",
+  measurementId: "G-1WB1RNRQ53"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth()
+
+export {app, auth}
